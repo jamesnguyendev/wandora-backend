dec4dc181e3f7f78e516329c49768be0fb412d31
jamesnguyendev <jamesnguyen.devs@gmail.com>
2025-10-31 13:09:28 +0700

    chore: update error.middle

--- src/api/middlewares/error.middleware.ts
+++ src/api/middlewares/error.middleware.ts
@@ -8,6 +8,6 @@ export const errorHandler = (
 ) => {
   console.error(err); // log ra console
   res.status(err.status || 500).json({
-    message: err.message || "Internal Server Error",
+    message: err.message || "Internal Server Error.",
   });
 };
