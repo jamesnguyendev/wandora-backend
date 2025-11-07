91ff1063543b232aa4d9f9fef572d81d2f874054
jamesnguyendev <jamesnguyen.devs@gmail.com>
2025-11-07 14:52:35 +0700

    fix: update auth controller

--- src/api/controllers/auth.controller.ts
+++ src/api/controllers/auth.controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
 import * as authService from "../../services/auth.service";
+import { ApiResponse } from "../../utils/apiResponse";
 
 export const register = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
-    const user = await authService.registerUser(email, password);
-    res.status(201).json({ id: user.id, email: user.email, role: user.role });
+    await authService.registerUser(email, password);
+    return ApiResponse.success(res, "Successfully registered");
   } catch (err: any) {
     res.status(400).json({ error: err.message });
   }
